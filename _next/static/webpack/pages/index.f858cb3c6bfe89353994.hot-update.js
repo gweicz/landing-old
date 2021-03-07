881ccd29a1054f79665539ca06d0740b30306efc
rixcian <rostislav.kremecek@protonmail.com>
2021-03-07 18:14:28 +0100

    Updates

---
+++ _next/static/webpack/pages/index.f858cb3c6bfe89353994.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./public/data/posts.json":
+/*!********************************!*\
+  !*** ./public/data/posts.json ***!
+  \********************************/
+/*! exports provided: 0, 1, 2, 3, 4, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"[{\\\"title\\\":\\\"Ethereum.org: Český překlad oficiální Ethereum stránky\\\",\\\"date\\\":\\\"07/03/2021\\\",\\\"url\\\":\\\"https://forum.gwei.cz/t/ethereum-org-cz-preklad/214\\\"},{\\\"title\\\":\\\"Kryptokalendář: Česko-slovenské krypto události (livestreamy, pokecy)\\\",\\\"date\\\":\\\"15/02/2021\\\",\\\"url\\\":\\\"https://forum.gwei.cz/t/kryptokalendar-cesko-slovenske-krypto-udalosti-livestreamy-pokecy/255\\\"},{\\\"title\\\":\\\"Pálíme Ether Call: Pravidelná diskuze o novinkách v Ethereu/DeFi\\\",\\\"date\\\":\\\"15/02/2021\\\",\\\"url\\\":\\\"https://forum.gwei.cz/t/serie-palime-ether-kazde-utery-17-00/235\\\"},{\\\"title\\\":\\\"Inkubátor Call: Pravidelná diskuze o nápadech na DeFi projekty\\\",\\\"date\\\":\\\"11/02/2021\\\",\\\"url\\\":\\\"https://forum.gwei.cz/t/serie-inkubator-ctvrtek-18-00/215\\\"},{\\\"title\\\":\\\"Zahájení vývoje novinkového webu o kryptoměnách\\\",\\\"date\\\":\\\"10/02/2021\\\",\\\"url\\\":\\\"https://forum.gwei.cz/t/novinkovy-web-o-kryptomenach/232\\\"}]\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3B1YmxpYy9kYXRhL3Bvc3RzLmpzb24uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./public/data/posts.json\n");
+
+/***/ })
+
+})
