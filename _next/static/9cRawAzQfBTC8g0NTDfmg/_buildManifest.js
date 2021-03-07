c6dbd4deee532a19bfcdd7773ff8d30ae9cfcac9
rixcian <rostislav.kremecek@protonmail.com>
2021-03-07 21:01:31 +0100

    Updates

---
+++ _next/static/9cRawAzQfBTC8g0NTDfmg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7bb5e6f2fc935ce24f7b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-01bf5f7678408885b2a1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
