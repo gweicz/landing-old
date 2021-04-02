e7db08e30db6ac45a81d610642ab691b3e7dbc53
tree <tree@hidden.cz>
2021-04-02 05:07:55 +0300

    refactor: change favicon #7

--- components/Layout/LayoutHead.js
+++ components/Layout/LayoutHead.js
@@ -11,7 +11,7 @@ const LayoutHead = () => {
       <title>Gwei.cz | CZ/SK Ethereum/DeFi komunita</title>
 
       <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
   )
 }
